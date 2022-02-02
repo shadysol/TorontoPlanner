fb1b4cd43e69856bb1d2c42f956195fd09ed74b3
shadysol <95047476+shadysol@users.noreply.github.com>
2022-02-01 21:28:49 -0500

    form and routes

--- app.js
+++ app.js
@@ -6,11 +6,25 @@ const path = require('path');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
     res.render('home')
   })
+
+app.get('/planner', (req, res) => {
+
+    res.render('plannerForm', )
+  })
+
+app.post('/plannerForm', (req, res) => {
+  const test = req.body.location
+  console.log(test)
+ // res.render('show',{location})
+ //console.log({location})
+} )
 
 app.listen(3000, () => {
     console.log(`Serving on port 3000`)
