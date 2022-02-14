6c3f90b682ba5f4423ab345bf466fa565d27d56a
shadysol <95047476+shadysol@users.noreply.github.com>
2022-02-14 17:31:03 -0500

    plan views created

--- app.js
+++ app.js
@@ -14,11 +14,13 @@ const yelp = require('yelp-fusion')
 const apiKey = '1dTgDkb9wviaRzrO8lkUYpLNZDwKiTXaA1hA244ln2oHLBBWE9_jqdn_lksJn5jVgXBAhwueXqwBADSNGqnLLR4r7163c3FHRgZ7L4ih1g6hv7jCptvbOakV2u_1YXYx'
 
 const searchRequest = {
-  term: 'food',
-  latitude: '43.75760726547434',
-  longitude: '-79.59891379095171',
-  radius: 2000,
-  sort_by: 'rating'
+  term: 'restaurants',
+  location: '7 Grenville Street, Toronto',
+ // latitude: '43.75760726547434',
+ // longitude: '-79.59891379095171',
+  radius: 500,
+  sort_by: 'rating',
+  limit: 5
 
 };
 
@@ -37,17 +39,20 @@ app.get('/planner', (req, res) => {
     res.render('plannerForm', )
   })
 
-  app.post('/plannerForm', (req, res) => {
+  app.post('/planner', (req, res) => {
 
 
     client.search(searchRequest)
   .then((response) => {
-    console.log(response.jsonBody);
+    var resturaunts = [];
+    console.log(response.jsonBody.businesses[1].categories);
+     resturaunts = response.jsonBody.businesses
+    res.render('plan', {resturaunts})
   })
   .catch((error) => {
     console.log(error);
   });
-    //res.render('plannerForm', )
+   
   })
 
 
